fdf11f975da79f87414fc18c4782936df55fd530
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-08-15 12:33:45 +0530

    auth added

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,17 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseUrl: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_API_ID,
+  apiKey: 'AIzaSyCk546itKt_3k8MprqW5SlYNSRA_gZ0qZc',
+  authDomain: 'message-app-77f91.firebaseapp.com',
+  databaseUrl: 'https://message-app-77f91.firebase.io.com',
+  projectId: 'message-app-77f91',
+  storageBucket: 'message-app-77f91.appspot.com',
+  messagingSenderId: '388301541267',
+  appId: '1:388301541267:web:9860f31b8b8bfe7c69a266',
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../Firebase';
+import './register.css';
 
 function Register() {
   const [data, setData] = useState({
@@ -7,16 +10,38 @@ function Register() {
     email: '',
     password: '',
   });
-
+  const { name, email, password } = data;
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: [e.target.value] });
   };
 
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      debugger;
+      alert('all field are required ');
+    }
+    try {
+      const createUser = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      debugger;
+    } catch (err) {
+      console.log('error');
+    }
+  };
+
   return (
-    <div>
+    <div className='main-frm-cnt'>
+      <div className='heading-cnt'>
+        <p className='form-heading'>Register as a user.</p>
+      </div>
       <form
         onSubmit={(e) => {
-          e.preventDefault();
+          handelSubmit(e);
         }}
         className='reg-form'
       >
@@ -48,11 +73,14 @@ function Register() {
           name='password'
           className='reg-frm-inp'
         />
-        <NavLink to='/login' className='navigate-btn' type='button'>
-          Login
-        </NavLink>
-        <button type='submit' className='btn'>
-          Register
+        <div className='frm-btn'>
+          <NavLink to='/login' className='navigate-btn' type='button'>
+            Login
+          </NavLink>
+        </div>
+
+        <button type='submit' className='submit-btn'>
+          Submit
         </button>
       </form>
     </div>

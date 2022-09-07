f7174da6eedbc4d2b9d80a0b7fd0468b7a247bb0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-09-07 18:43:01 +0530

    upload function created

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCk546itKt_3k8MprqW5SlYNSRA_gZ0qZc',
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
+import { storage } from '../../Firebase';
 
 const Profile = () => {
   const [img, setImg] = useState('');
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const imgRef = ref(storage, `avatar/${new Date().getTime()}.${img.name}`);
+      const snap = await uploadBytes(imgRef, img);
+    } catch (err) {
+      console.error('error');
+    }
+  };
+
   return (
     <div>
       <div className='file-container'>
         <label>upload your photo</label>
-        <form>
+        <div className='img-container'>
+          <img src={img} alt='avatar' />
+        </div>
+        <form
+          className='form-container'
+          onSubmit={(e) => {
+            handelSubmit(e);
+          }}
+        >
           <input
             type='file'
             accept='image/'
             id='photo'
-            onChange={(e) => setImg(e.target.file[0])}
+            onChange={(e) => setImg(e.target.files[0])}
           />
           <button type='submit'>upload</button>
         </form>

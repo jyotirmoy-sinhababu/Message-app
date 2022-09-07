432c58d2f98eec95d13f6315d6d08223bd33d3d7
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-09-07 16:39:41 +0530

    profile page created

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [img, setImg] = useState('');
+  return (
+    <div>
+      <div className='file-container'>
+        <label>upload your photo</label>
+        <form>
+          <input
+            type='file'
+            accept='image/'
+            id='photo'
+            onChange={(e) => setImg(e.target.file[0])}
+          />
+          <button type='submit'>upload</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;

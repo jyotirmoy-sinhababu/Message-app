5ebcaac23111651e73047668bd18feff414f9252
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-09-08 17:26:51 +0530

    we can upload image

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 import { storage } from '../../Firebase';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import './profile.css';
 
 const Profile = () => {
-  const [img, setImg] = useState('');
+  const [img, setImg] = useState();
 
   const handelSubmit = async (e) => {
     e.preventDefault();
-    const imgRef = ref(storage, `${new Date().getTime()}-${img.name}`);
+    const imgRef = ref(
+      storage,
+      `CameraAltIcon/${new Date().getTime()}-${img.name}`
+    );
     const snap = await uploadBytes(imgRef, img);
     console.log(snap);
   };
@@ -22,13 +26,19 @@ const Profile = () => {
             handelSubmit(e);
           }}
         >
-          <input
-            type='file'
-            accept='image'
-            id='photo'
-            onChange={(e) => setImg(URL.createObjectURL(e.target.files[0]))}
-          />
-          <button type='submit'>upload</button>
+          <label className='label-container'>
+            {<img className='img-label' src={img} alt='set image' />}
+            <input
+              type='file'
+              accept='image'
+              id='photo'
+              onChange={(e) => setImg(URL.createObjectURL(e.target.files[0]))}
+            />
+          </label>
+
+          <button className='upload-btn' type='submit'>
+            upload
+          </button>
         </form>
       </div>
     </div>
